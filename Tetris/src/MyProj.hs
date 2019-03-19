cea20dbe4de5bd2c54cda47f22387c1017656070
Lena <Derinhelm@yandex.ru>
2019-03-19 13:53:49 +0300

    prototypos compiling

--- Tetris/src/MyProj.hs
+++ Tetris/src/MyProj.hs
@@ -15,16 +15,16 @@ funLineAny f str1 = any f str1
 
 
 funFieldAll :: (Cell -> Bool) -> Field -> Bool -- проверка, что все поле удовлетворяет какому-то условию
-funFieldAll f field = all f field 
+funFieldAll f field = all (funLineAll f) field 
 
 funFieldAny :: (Cell -> Bool) -> Field -> Bool -- проверка, что в поле есть элементы, удовлетворяющие условию
-funFieldAny f field = any f field 
+funFieldAny f field = any (funLineAny f) field 
 
 createNextFigure::Int -- получение номера новой следующей фигуры - пока не реализовано
 createNextFigure = 1 
 
 shiftFigure :: Field -> Field --сдвиг фигуры(на 1 вниз + по клавишам) - пока не реализовано
-shiftFigure x checkFlyFigure = x
+shiftFigure x  = x
 
 addFigure :: Field -> NumberFigure -> Field -- падение новой фигуры  - пока не реализовано
 addFigure field _ = field
@@ -32,25 +32,31 @@ addFigure field _ = field
 deleteLines :: Field -> Int -> (Field, Int) -- удаление линии со всеми заполненными  - пока не реализовано
 deleteLines f r = (f, r)
 
-checkEnd :: Field -> Bool -- проверка, что фигурка не может дальше лететь
+checkEnd :: Field -> Bool -- проверка, что игра закончена
 checkEnd  field | funFieldAny (\c -> (typeCell c) == 3) field = True -- не удалось построить новое поле, значит конец
-                | funFieldAny (\c -> and ((typeCell c) == 1) (typeCell (lower c field) == 0) ) field = True
+                | funFieldAny (\c -> ((typeCell c) == 1) && (typeCell (lowerCell c field) == 0) ) field = True
                 | otherwise = False
+
+checkLand :: Field -> (Bool, Field) --проверка, что фигурка приземлилась 
+checkLand t = (True, t)
             
 gameLoop :: Field -> Int -> NumberFigure -> IO Int --игровой цикл 
-gameLoop oldField oldResult nextFigure | isLand | checkEnd newField1 = do 
-                                                        draw oldField
-                                                        return oldResult
-                                                | otherwise =  do
-                                                        draw newField1
-                                                        --можно рисовать и oldField,отличаются типом клетки(незаметно при рисовании) 
-                                                        draw newField2
-                                                        gameLoop (addFigure nextFigure) newResult (createNextFigure)
-                                        | otherwise = do
-                                            draw oldField
-                                            gameLoop (shiftFigure newField1) newResult nextFigure
+gameLoop oldField oldResult nextFigure = if isLand 
+                                                then if checkEnd newField1 
+                                                        then do
+                                                                drawField oldField
+                                                                return oldResult
+                                                        else do
+                                                                drawField newField1
+                                                                --можно рисовать и oldField,отличаются типом клетки(незаметно при рисовании) 
+                                                                drawField newField2
+                                                                gameLoop (addFigure newField2 nextFigure) newResult (createNextFigure)
+                                                else 
+                                                        do
+                                                                drawField oldField
+                                                                gameLoop (shiftFigure newField1) newResult nextFigure
     where
-        (isLand, newField1) = checkLandFigure oldField
+        (isLand, newField1) = checkLand oldField
         (newField2, newResult) = deleteLines newField1 oldResult
 
 
